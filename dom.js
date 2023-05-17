58d16db7a335041208dfbd10c0ec9d64f2ebf08a
rashmibhandari1497 <bhandarirasmi999>
2023-05-17 18:24:36 +0530

    ok

---
+++ dom.js
@@ -0,0 +1,6 @@
+// EXAMINE THE DOCUMENT
+//console.dir(document);
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+var item= document.querySelector('.list-group-item');
+item.style.color= 'green';
