4301b4b31c31f605ae00cadcedaba90ffb2dc750
rashmibhandari1497 <bhandarirasmi999>
2023-05-17 18:36:44 +0530

    modified

--- dom.js
+++ dom.js
@@ -1,6 +1,11 @@
 // EXAMINE THE DOCUMENT
 //console.dir(document);
-var header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px #000';
-var item= document.querySelector('.list-group-item');
-item.style.color= 'green';
+//var header = document.getElementById('main-header');
+//header.style.borderBottom = 'solid 3px #000';
+//var item= document.querySelector('.list-group-item');
+//item.style.color= 'green';
+var items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = 'yellow';
+for (var i = 0 ; i<items.length ; i++){
+    items[i].style.fontWeight = 'bold';
+}
