5328556b11b5f324efa9d358075e8579e27750e5
rashmibhandari1497 <bhandarirasmi999>
2023-05-18 22:30:02 +0530

    querryselectorall

--- dom.js
+++ dom.js
@@ -10,9 +10,18 @@
 //li[1].textContent = 'Hello 2';
 
 //QUERRYSELECTOR
-var item = document.querySelector('.list-group-item:nth-child(2)');
-item.style.backgroundColor = 'green';
-var itemThird = document.querySelector('.list-group-item:nth-child(3)');
-itemThird.style.display = 'none';
- 
+//var item = document.querySelector('.list-group-item:nth-child(2)');
+//item.style.backgroundColor = 'green';
+//var itemThird = document.querySelector('.list-group-item:nth-child(3)');
+//itemThird.style.display = 'none';
+
+//QUERRYSELECTORALL
+var item = document.querySelectorAll('.list-group-item');
+item[1].style.color = 'green';
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for (var i = 0 ; i<odd.length;i++){
+    odd[i].style.backgroundColor = 'green';
+
+
+}
 
