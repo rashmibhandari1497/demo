3686d8aff9b1d445e5254b57ddd9d7432aa86e11
rashmibhandari1497 <bhandarirasmi999>
2023-05-18 21:09:10 +0530

    Elementby tag or class name

--- dom.js
+++ dom.js
@@ -4,8 +4,12 @@
 //header.style.borderBottom = 'solid 3px #000';
 //var item= document.querySelector('.list-group-item');
 //item.style.color= 'green';
+//ELEMENTBYCLASSNAME
 var items = document.getElementsByClassName('list-group-item');
 items[2].style.backgroundColor = 'yellow';
 for (var i = 0 ; i<items.length ; i++){
     items[i].style.fontWeight = 'bold';
 }
+//ELEMENTBYTAGNAME
+var li = document.getElementsByTagName('li');
+li[1].textContent = 'Hello 2';
